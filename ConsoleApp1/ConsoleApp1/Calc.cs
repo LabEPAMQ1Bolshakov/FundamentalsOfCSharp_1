02f0a102b5f10d38350b05a370aeed2d30d8b08c
Алексей Большаков <lexeybad@yandex.ru>
2018-03-24 15:29:09 +0300

    Still without methods

--- ConsoleApp1/ConsoleApp1/Calc.cs
+++ ConsoleApp1/ConsoleApp1/Calc.cs
@@ -10,40 +10,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string stay;
-            do
+            string stayHere = "N";
+            while (stayHere != null)
             {
                 Console.WriteLine("Choose the type of calculation. Enter +, or -, or *, or / :");
                 string typeOfCalc = Console.ReadLine();
 
                 Console.WriteLine("Enter the first number:");
                 string str = Console.ReadLine();
-                double firstNum = double.Parse(str);
+                double firstNum = float.Parse(str);
 
                 Console.WriteLine("Enter the second number:");
                 str = Console.ReadLine();
-                double secondNum = double.Parse(str);
+                double secondNum = float.Parse(str);
 
-                if (typeOfCalc == "+")
+                if (typeOfCalc.Equals("+"))
                 {
                     Console.WriteLine("{0} + {1} = {2}", firstNum, secondNum, firstNum + secondNum);
                 }
-                else if (typeOfCalc == "-")
+                else if (typeOfCalc.Equals("-"))
                 {
                     Console.WriteLine("{0} - {1} = {2}", firstNum, secondNum, firstNum - secondNum);
                 }
-                else if (typeOfCalc == "*")
+                else if (typeOfCalc.Equals("*"))
                 {
                     Console.WriteLine("{0} * {1} = {2}", firstNum, secondNum, firstNum * secondNum);
                 }
-                else if (typeOfCalc == "/")
+                else if (typeOfCalc.Equals("/"))
                 {
                     Console.WriteLine("{0} / {1} = {2}", firstNum, secondNum, firstNum / secondNum);
                 }
 
-                Console.WriteLine("Would you like to make a new calculation?(enter 'y' to continue):");
-                stay = Console.ReadLine();
-            } while (stay == "y");
+                Console.WriteLine("Would you like to make a new calculation?");
+                Console.WriteLine("Press Enter to continue");
+                Console.WriteLine("Enter N to exit");
+                stayHere = Console.ReadLine();
+                if (stayHere.Equals("N")) break;                                                            
+                            
+            } 
         }
     }
 }
